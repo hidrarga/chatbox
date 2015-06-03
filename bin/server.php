23f43b1a00d52cc62067b6b5eb724899668d2845
HgO <hgo@batato.be>
2015-06-03 17:43:17 +0200

    Merge FlashPolicy server with the Websocket server
    This probably fix a bug, to be tested with IE7/8

--- bin/server.php
+++ bin/server.php
@@ -1,18 +1,39 @@
 <?php
     use Ratchet\Server\IoServer;
+    use Ratchet\Server\FlashPolicy;
     use Ratchet\Http\HttpServer;
     use Ratchet\WebSocket\WsServer;
+    
+    use React\EventLoop\Factory;
+    use React\Socket\Server as Reactor;
+    
     use ChatBox\Chat;
     
     require dirname(__DIR__).'/vendor/autoload.php';
     
-    $server = IoServer::factory(
+    $loop = Factory::create();
+ 
+    // Our existing server socket
+    $webSocket = new Reactor($loop);
+    $webSocket->listen(8080, '0.0.0.0');    
+    $server = new IoServer(
         new HttpServer(
             new WsServer(
                 new Chat()
             )
-        ),
-        8080
+        ), $webSocket
     );
-    
-    $server->run();
+ 
+    // for allowing flash policy
+    $flashSocket = new Reactor($loop);
+    $flashSocket->listen(843, '0.0.0.0');   //The Flash player always tries port 843 first;
+    $policy = new FlashPolicy;
+    $policy->addAllowedAccess('*', 80);
+    $policy->addAllowedAccess('*', 8080);
+ 
+    $server = new IoServer(
+        $policy,                    
+        $flashSocket
+    );
+ 
+    $loop->run();
